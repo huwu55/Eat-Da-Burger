76011e24301319c51286a6e1a81dbd7a06c14c0f
Huiling Wu <huwu@ucdavis.edu>
2019-01-10 15:45:01 -0800

    added jawsdb connection

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # burger
+
+A burger logger with MySQL, Node, Express, EJS and a homemade ORM (yum!).
+
--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,22 @@ require("dotenv").config();
 
 var mysql_pw = process.env.MYSQL;
 
-var connection = mysql.createConnection({
-    host: "localhost",
+var connection;
 
-    port: 3306,
-
-    user: "root",
-
-    password: mysql_pw,
-    database: "burgers_db"
-});
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+    connection = mysql.createConnection({
+        host: "localhost",
+    
+        port: 3306,
+    
+        user: "root",
+    
+        password: mysql_pw,
+        database: "burgers_db"
+    });    
+}
 
 module.exports = connection;
